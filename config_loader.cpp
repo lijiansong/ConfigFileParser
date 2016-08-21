214a98f23a4f9b1295f1a3b2d0ed0bd73f3c8dc7
Json Lee <jiansongli426@gmail.com>
2016-08-21 21:24:41 +0800

    json lee add include parsing part

---
+++ config_loader.cpp
@@ -0,0 +1,109 @@
+
+#include "config_loader.h"
+
+#include <stdlib.h>
+#include <list>
+#include <string>
+#include <iostream>
+#include <set>
+
+
+#include "config_map.h"
+#include "util.h"
+
+ConfigLoader::ConfigLoader() {}
+
+ConfigLoader::~ConfigLoader() {}
+
+void print_lines(StringList & lines) 
+{
+	StringList::iterator it = lines.begin();
+	int i=0;
+	while (it != lines.end()) 
+	{
+		std::cout << i++ << ": " << (*it);
+		it++;
+	}
+}
+
+void ConfigLoader::Load(const std::string& file_name, StringList& lines) 
+{
+	// Load the main file
+	LoadInternal(file_name, lines, lines.begin());
+
+	// Set of already loaded includes
+	std::set<std::string> loaded;
+
+	// Load includes
+	StringList::iterator it = lines.begin();
+	while (it != lines.end()) 
+	{
+		std::string line = (*it);
+
+		std::string f = ExtractInclude(line);
+
+		if (!f.empty()) 
+		{
+			if (loaded.find(f) != loaded.end()) 
+			{
+				// Already loaded this include, there is a loop
+				std::cout << "loop in includes " << f;
+			exit(1);
+			}
+
+			loaded.insert(f);
+
+			it = lines.erase(it);
+			it = LoadInternal(f, lines, it);
+		} 
+		else 
+		{
+			it++;
+		}
+	}
+}
+
+std::string ConfigLoader::ExtractInclude(const std::string& line) 
+{
+	if (line.substr(0, 8) == "#include") 
+	{
+		std::string s = line.substr(9);
+		return ConfigMap::RemoveQuotes(&s);
+	}
+	return "";
+}
+
+StringList::iterator ConfigLoader::LoadInternal(
+    const std::string& file_name, StringList& lines,
+    StringList::iterator it) 
+{
+	std::fstream fin;
+	std::cout<< "loading file " << file_name;
+
+	fin.open(file_name.c_str(), std::ios::in);
+
+	if (!fin.good()) 
+	{
+		std::cout << "could not open " << file_name;
+		exit(1);
+	}
+
+	StringList::iterator first_it = it;
+
+	bool first = true;
+
+	std::string line;
+	while (!fin.eof()) 
+	{
+		getline(fin, line);
+
+		::iterator itx = lines.insert(it, line);
+		if (first) 
+		{
+			first = false;
+			first_it = itx;
+		}
+	}
+	fin.close();
+	return first_it;
+}
---
+++ config_loader.h
@@ -0,0 +1,32 @@
+
+#ifndef CONFIGLOADER_H_
+#define CONFIGLOADER_H_
+
+#include <fstream>
+#include <string>
+#include <list>
+#include <map>
+
+typedef std::list<std::string> StringList;
+
+// Loads a configuration file from disk, recursively loading included files.
+class ConfigLoader {
+public:
+  ConfigLoader();
+  ~ConfigLoader();
+
+  void Load(const std::string& file_name, StringList& out);
+
+private:
+  // Disallow copy and assign
+  explicit ConfigLoader(const ConfigLoader& copy_from_me);
+  void operator=(const ConfigLoader&);
+
+  StringList::iterator LoadInternal(const std::string& file_name,
+      StringList& lines, StringList::iterator it);
+
+  std::string ExtractInclude(const std::string& line);
+  int CleanLine(std::string& line, char *out);
+};
+
+#endif /* CONFIG_CONFIGLOADER_H_ */
